2686f175e385b1daef9dfbce0f9d1f362454dddd
ayda <aydashamma4@gmail.com>
2025-03-01 04:31:22 -0800

    created home sidebar layout

---
+++ src/components/home/home.jsx
@@ -0,0 +1,13 @@
+import SideBar from "./sideBar";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="w-[240px] border-r-1 border-[#ccc]">
+        <SideBar />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
